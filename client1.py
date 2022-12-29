803ae41ca14577308f2652426ea7554727773763
yasar cosar <ycosarb06@gmail.com>
2022-12-30 00:11:44 +0300

    Add files via upload

---
+++ client1.py
@@ -0,0 +1,31 @@
+import socket       
+from threading import *
+
+# Socket oluşturulması
+s = socket.socket()          
+ 
+# Bağlanılacak adres ve port
+host = "127.0.0.1"
+port = 12345        
+
+s.connect((host,port))
+
+while True:
+
+    clientMessage = input("Mesaj :")
+    
+    s.send(clientMessage.encode('utf8'))
+    
+    
+
+    def recvMessage():
+        while True:
+            serverMessage = s.recv(1024).decode('utf8')
+            print(serverMessage)
+
+
+
+
+    recThread = Thread(target=recvMessage)
+    recThread.daemon=True
+    recThread.start()
